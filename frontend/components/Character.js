801097444752cfef8cda3141cabdefb1f395544c
Joberthy Mandiangu <joberthymambu@gmail.com>
2024-05-02 23:04:33 -0500

    Updates

--- frontend/components/Character.js
+++ frontend/components/Character.js
@@ -1,35 +1,26 @@
 import React, { useState } from 'react';
 
-function Character({ character, planet }) {
+function Character({ character }) {
   // State to hold whether the homeworld is rendering or not
-  const [showPlanet, setShowPlanet] = useState(false);
-
+  const [ShowPlanet, setShowPlanet] = useState(false);
   // Toggle click handler to show or remove the homeworld
   const togglePlanet = () => {
-    setShowPlanet(!showHomeworld);
+    setShowPlanet(!showPlanet);
   };
+
 
   return (
     <div className="character-card">
       <h3 className="character-name">{character.name}</h3>
       <p>
-        Planet: 
-        <span className="character-planet">{planet}</span>
+        Planet:  
+        <span className="character-planet">{}</span>
       </p>
       
-      {/* <p>Gender: {character.gender}</p>
-      <p>Birth Year: {character.birth_year}</p> */}
-      {/* <button onClick={toggleHomeworld}>
-        {showHomeworld ? 'Hide Homeworld' : 'Show Homeworld'}
+      {/* <button onClick={togglePlanet}>
+        {showPlanet ? 'Hide Planet' : 'Show Planet'}
       </button>
-      {showHomeworld && ( 
-        <div className="homeworld">
-          <h4>Homeworld</h4>
-          <p>Name: {character.homeworld.name}</p>
-          <p>Climate: {character.homeworld.climate}</p>
-          <p>Population: {character.homeworld.population}</p>
-        </div>
-      {/* )} */}
+      */}
     </div>
   );
 }
